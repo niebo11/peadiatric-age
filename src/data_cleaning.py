9be2fa93b222b6c598e4a45023e77f37041e5d4e
niebo11 <zhangniebo@gmail.com>
2020-12-18 22:58:06 +0100

    added treat_obesity

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -4,17 +4,27 @@ import seaborn as sn
 import pandas as pd
 from collections import Counter
 
-rawData = pd.read_csv("data/preprocessed/COPEDICATClinicSympt_DATA_2020-12-17_1642.csv", header=0, delimiter=',')
 
-attributes = rawData.columns.tolist()
+# This function cleans the column obesity, for each NaN value is changed for "no" otherwise is changed to "yes"
+def treat_obesity(data):
+    obesity = [item.lower() for item in rawData['obesity'].fillna('no')]
+    obesity = ["si" if item != "no" else item for item in obesity]
+    data['obesity'] = obesity
+    return data
 
-dropAttributes = ["id", "participant_id", "recruit_date", "postal_code", "province", "family_country",
-              "row_school", "sports_type", "m2", "floor_level", "rooms","persons_home",
-              "survey_type", "cxr", "ct", "sero_date", "cxr_date", "pcr_date", "pcr_type", "antigenic_date",
-              "discharge_date", "adm_date", "comments", "survey_end_date"]
 
-rawData = rawData.drop(dropAttributes, axis=1)
+if __name__ == '__main__':
+    rawData = pd.read_csv("data/preprocessed/COPEDICATClinicSympt_DATA_2020-12-17_1642.csv", header=0, delimiter=',')
+    attributes = rawData.columns.tolist()
+    rawData = treat_obesity(rawData)
 
-# TODO modificar els housemember symptoms
-# TODO simptomatology_date
-# TODO eliminar els first?
+    dropAttributes = ["id", "participant_id", "recruit_date", "postal_code", "province", "family_country",
+                      "row_school", "sports_type", "m2", "floor_level", "rooms", "persons_home",
+                      "survey_type", "cxr", "ct", "sero_date", "cxr_date", "pcr_date", "pcr_type", "antigenic_date",
+                      "discharge_date", "adm_date", "comments", "survey_end_date"]
+
+    rawData = rawData.drop(dropAttributes, axis=1)
+
+    # TODO modificar els housemember symptoms
+    # TODO simptomatology_date
+    # TODO eliminar els first?
