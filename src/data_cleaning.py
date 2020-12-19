fcc3f7621e0570f03f8329258f95c4dff4c65067
xLizart <aleix.sarrocarf@gmail.com>
2020-12-19 19:23:59 +0100

    covid treat #6 pushpuysh

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -107,11 +107,37 @@ def treat_na_int(data):
     data["school_confirmed"] = data["school_confirmed"].fillna(0)
     data["school_confirmed"] = data["school_confirmed"].astype('int32')
 
+    data["flu_binary"] = data["flu_binary"].fillna(0)
+    data["flu_binary"] = data["flu_binary"].astype('int32')
+
+    data["vaccines_binary"] = data["vaccines_binary"].fillna(0)
+    data["vaccines_binary"] = data["vaccines_binary"].astype('int32')
+
     # for columna in [item for item in data.columns.tolist() if data[item].dtype == "float64"]:
     #     data[columna] = data[columna].fillna(-1)
     #     data[columna] = data[columna].astype('int32')
 
     return data
+
+def treat_symptoms(data, desc, yn):
+
+    for columna in desc:
+        data[columna] = data[columna].fillna(3)
+        data[columna] = data[columna].astype('int32')
+
+    for columna in yn:
+        data[columna] = data[columna].fillna(0)
+        data[columna] = data[columna].astype('int32')
+
+    return data
+
+def treat_virus(data, desc):
+    for columna in desc:
+        data[columna] = data[columna].fillna(3)
+        data[columna] = data[columna].astype('int32')
+
+    return data
+
 
 def treat_covid(data):
 
@@ -122,9 +148,10 @@ def treat_covid(data):
     data["antigenic_result"] = data["antigenic_result"].fillna(0)
     data["antigenic_result"] = data["antigenic_result"].astype('int32')
 
-    covid = []
+
     pcr_result = [item for item in data["pcr_result"]]
     ant_result = [item for item in data["antigenic_result"]]
+    covid = pcr_result
     for i in range(len(data["pcr_result"].tolist())):
         if pcr_result[i] == 1:
             covid[i] = 1
@@ -133,9 +160,30 @@ def treat_covid(data):
         else:
             covid[i] = 0
 
-    data["covid"] = covid
+    data.insert(0,"covid", covid)
 
     return data
+
+def treat_coviral(data):
+
+    data["coviral_binary"] = data["coviral_binary"].fillna(9)
+    data["coviral_binary"] = data["coviral_binary"].astype('int32')
+
+    data["coviral_type"] = data["coviral_type"].fillna(0)
+    data["coviral_type"] = data["coviral_type"].astype('int32')
+
+    data["coviral"] = data["coviral_type"] + data["coviral_binary"]
+
+    data["bacterial_infection"] = data["bacterial_infection"].fillna(3)
+    data["bacterial_infection"] = data["bacterial_infection"].astype('int32')
+
+    data["antigenic_performed"] = data["antigenic_performed"].fillna(0)
+    data["antigenic_performed"] = data["antigenic_performed"].astype('int32')
+    data["pcr_performed"] = data["pcr_performed"].fillna(0)
+    data["pcr_performed"] = data["pcr_performed"].astype('int32')
+
+    return data
+
 
 if __name__ == '__main__':
     rawData = pd.read_csv("data/preprocessed/COPEDICATClinicSympt_DATA_2020-12-17_1642.csv", header=0, delimiter=',')
@@ -145,20 +193,41 @@ if __name__ == '__main__':
     rawData = treat_na_int(rawData)
     rawData = treat_school_symptoms(rawData)
     rawData = treat_covid(rawData)
+    rawData = treat_coviral(rawData)
+
+    viruses = ['vrs_result', 'adeno_result', 'flu_a_result', 'flu_b_result']
+    rawData = treat_virus(rawData, viruses)
 
     attributes = rawData.columns.tolist()
     attributes_first = [item for item in attributes if item.find("_first") != -1]
     rawData = treat_obesity(rawData)
     rawData = treat_first(rawData, attributes_first, attributes)
 
-    dropAttributes = ["id", "bus", "participant_id", "recruit_date", "postal_code", "province", "family_country",
-              "row_school", "sports_type", "m2", "floor_level", "rooms","persons_home",
-              "survey_type", "cxr", "ct", "sero_date", "cxr_date", "pcr_date", "pcr_type", "antigenic_date",
-              "discharge_date", "adm_date", "comments", "survey_end_date", "housemember_symptoms___2",
-              "housemember_symptoms___3", "housemember_symptoms___4", "housemember_symptoms___5",
-              "housemember_symptoms___1", "school_symptoms_member___4", "school_symptoms_member___5",
-              "name_initials_of_the_inter", "otherviruses_date", "simptomatology_date", "date_fever", "symptomatology_date",
-              "thoracic_ct_date", "adm_date", "discharge_date", "flu_date", "survey_end_date"] + attributes_first
+    dropAttributes = ['othervirus_performed',"id", "bus", "participant_id", "recruit_date", "postal_code", "province", "family_country",
+        "row_school", "sports_type", "m2", "floor_level", "rooms","persons_home",
+        "survey_type", "cxr", "ct", "sero_date", "cxr_date", "pcr_date", "pcr_type", "antigenic_date",
+        "discharge_date", "adm_date", "comments", "survey_end_date", "housemember_symptoms___2",
+        "housemember_symptoms___3", "housemember_symptoms___4", "housemember_symptoms___5",
+        "housemember_symptoms___1", "school_symptoms_member___4", "school_symptoms_member___5",
+        "name_initials_of_the_inter", "otherviruses_date", "simptomatology_date", "date_fever", "simptomatology_date",
+        "thoracic_ct_date", "adm_date", "discharge_date", "flu_date", "survey_end_date", "school_symptoms", "highest_fever",
+        "total_days_fever", "end_fever", "home_confirmed","sero","sero_type_response","sero_method","sero_response",
+        "sero_type_response_2","cxr2","ct2","sat_hb_o2","sat_hb_o2_value", 'other_simptomatology',
+        'other_simptomatology_3','other_simptomatology_4','other_simptomatology_lliure', 'final_diagnosis_text',
+        'adm_hospital','picu_adm','final_diagnosis_text','final_diagnosis_code','final_outcome','other_viruses_text',
+        'clinical_and_diagnosis_data_at_the_admission_time_complete', 'other', 'final_classification_of_th',
+        'final_outcome_complete','ag_test_mark', 'bacterial_type', 'coviral_type', 'coviral_binary', 'antigenic_sample',
+        ] + attributes_first
+
+    symptomsDesc = ['fever', 'tos', 'crup', 'dysphonia', 'resp', 'tachypnea', 'wheezing', 'crackles',
+                'odynophagia', 'nasal_congestion', 'fatiga', 'headache', 'conjuntivitis', 'ocular_pain', 'gi_symptoms',
+                'abdominal_pain', 'vomiting', 'dyarrea', 'adenopathies', 'hepato', 'splenomegaly', 'hemorrhagies',
+                'irritability', 'shock', 'taste_smell', 'smell']
+
+    symptomsYN = ['gi_symptoms', 'ausc_resp', 'dermatologic', 'rash', 'inflam_periferic', 'inflam_oral', 'neuro',
+                'confusion', 'seizures', 'nuchal_stiffness', 'hypotonia', 'peripheral_paralysis']
+
+    rawData = treat_symptoms(rawData, symptomsDesc, symptomsYN)
 
     # No comorbi_binary
     nCB_data = rawData[rawData.comorbi_binary == 0]
@@ -172,12 +241,16 @@ if __name__ == '__main__':
                 'kawasaki___1', 'kawasaki___2', 'kawasaki___3', 'inflammation___1', 'inflammation___2',
                 'inflammation___3', 'vih_others___1', 'vih_others___2', 'vih_others___3', 'comorbidities_complete']
 
+
     # comorbidities_complete unverified remove and incomplete try to complete
     rawData_nCB = nCB_data.drop(dropAttributes + diseases, axis=1)
-    rawData_nCB = rawData_nCB[(rawData_nCB.pcr_performed != 0) & (rawData_nCB.antigenic_performed != 0)]
+    rawData_nCB = rawData_nCB[(rawData_nCB.pcr_performed != 0) | (rawData_nCB.antigenic_performed != 0)]
+    print(rawData_nCB.columns.tolist())
+    print(rawData_nCB.shape)
 
     rawData_CB = treat_disease(diseases, rawData)
     rawData_CB = rawData_CB[rawData_CB.comorbi_binary == 1]
+
 
     rawData_CB = rawData_CB.drop(dropAttributes, axis=1)
 
@@ -187,5 +260,5 @@ if __name__ == '__main__':
 
 
 #print(rawData.columns.tolist())
-    # TODO fever, symptoms
+    # TODO  symptoms other
 
